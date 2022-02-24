dafb918c2d729c8a3e789ac78c666e799bd2782c
rakhat nazarkulova <rakhatnazarkulova@rakhats-MacBook-Air.local>
2022-02-24 02:26:05 -0600

    Initial commit

---
+++ src/com/company/Exception.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Exception extends java.lang.Exception {
+    public Exception(String massage){
+
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Person person = new Person();
+        person.setAge(20);
+        person.setName("Nursultan");
+
+    }
+}
